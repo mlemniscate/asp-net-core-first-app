6e853a514c597f73b995d05825d43fac6d470916
Milad Abdi <miladabdi1374@gmail.com>
2022-01-30 18:32:24 +0330

    Add Adapter pattern to program.

---
+++ Session03DesignPatterns/Session03DesignPatterns/Adapter/ILogger.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session03DesignPatterns.Adapter
+{
+    internal interface ILogger
+    {
+        void Log();
+    }
+
+    public class MyLogger : ILogger
+    {
+        public void Log()
+        {
+            
+        }
+    }
+
+    public class SpecialLogger
+    {
+        public void DoLog()
+        {
+            // This logger has a lot more features but don't has the same interface
+            // that our MyLogger has
+            // So we instead of replacing SpecialLogger in every where of project we
+            // can use Adapter pattern
+        }
+    }
+
+    // This is class adapter when we want to use an adapter to a interface
+    public class SpecialLoggerAdapter : SpecialLogger, ILogger
+    {
+        public void Log()
+        {
+            DoLog();
+        }
+    }
+
+    // This is object adapter we don't have multiple inheritance
+    public class SpecialLoggerAdapter2 : ILogger
+    {
+        private SpecialLogger logger = new SpecialLogger();
+        public void Log()
+        {
+            logger.DoLog();
+        }
+    }
+}
--- Session03DesignPatterns/Session03DesignPatterns/Program.cs
+++ Session03DesignPatterns/Session03DesignPatterns/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Session03DesignPatterns.AbstractFactory;
+using Session03DesignPatterns.Adapter;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,11 +10,16 @@ namespace Session03DesignPatterns
 {
     internal class Program
     {
+        // before adapter
+        //private static ILogger logger = new MyLogger();
+        // after adapter
+        private static ILogger logger = new SpecialLoggerAdapter();
         static void Main(string[] args)
         {
-            IRepositoryFactory repositoryFactory = new SqlRepositoryFactory();
-            ICategoryRepository categoryRepository = repositoryFactory.GetCategoryRepository();
+            /*IRepositoryFactory repositoryFactory = new SqlRepositoryFactory();
+            ICategoryRepository categoryRepository = repositoryFactory.GetCategoryRepository();*/
 
+            logger.Log();
         }
     }
 }
