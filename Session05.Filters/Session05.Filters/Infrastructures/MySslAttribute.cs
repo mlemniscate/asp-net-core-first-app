82d137b261526aa10da704ffea13094e3206a14e
MiladAbdi <miladabdi1374@gmail.com>
2022-02-09 15:04:45 +0330

    Add filter project

---
+++ Session05.Filters/Session05.Filters/Infrastructures/MySslAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Session05.Filters.Infrastructures
+{
+    public class MySslAttribute
+    {
+    }
+}
